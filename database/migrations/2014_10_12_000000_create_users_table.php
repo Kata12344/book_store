766a6ecfe9f6b790ebb4f18e166ae7b6936076dd
Kata12344 <95749629+Kata12344@users.noreply.github.com>
2022-09-13 20:10:54 +0200

    +rekordok

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -20,6 +21,9 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
+
+        User::create(['name'=>'Gizi','email'=>'diak1@gmail.com']);
+        User::create(['name'=>'Szilárd','email'=>'diak2@gmail.com']);
     }
 
     /**
--- database/migrations/2022_09_13_165933_create_books_table.php
+++ database/migrations/2022_09_13_165933_create_books_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Book;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -20,6 +21,9 @@ return new class extends Migration
             $table->integer('pieces')->default(10);
             $table->timestamps();
         });
+
+        Book::create(['author'=>'Pipellő','title'=>'Fantom','pieces'=>5]);
+        Book::create(['author'=>'Béni','title'=>'Magyarok','pieces'=>10]);
     }
 
     /**
